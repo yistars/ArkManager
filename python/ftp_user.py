eb1b88841a94dfb261d8e22392463f8c8021797c
冰砚炽 <36477925+Bing-Yanchi@users.noreply.github.com>
2020-05-08 23:58:42 +0800

    update

--- python/ftp_user.py
+++
@@ -1,7 +0,0 @@
-import _thread
-import ftp_server
-
-this_ftp = ftp_server.ftp_server()
-
-_thread.start_new_thread(this_ftp.run,())
-_thread.start_new_thread(this_ftp.add_user,('user','password',"C://",'elradfmwM'))
