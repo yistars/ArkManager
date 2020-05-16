4c8880019393587526370bd99fe1e901a241dd9c
冰砚炽 <36477925+Bing-Yanchi@users.noreply.github.com>
2020-05-16 14:13:15 +0800

    移动目录

--- python/module/ftp_server.py
+++
@@ -1,9 +0,0 @@
-from pyftpdlib.authorizers import DummyAuthorizer
-from pyftpdlib.handlers import FTPHandler, ThrottledDTPHandler
-from pyftpdlib.servers import FTPServer
-import yaml
-
-with open('config.yaml') as f:
-    data = yaml.load(f)
-
-    
