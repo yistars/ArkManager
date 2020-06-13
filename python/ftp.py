105ea3452563ced9d70a6cff1d1e1495ba216412
冰砚炽 <36477925+Bing-Yanchi@users.noreply.github.com>
2020-06-13 17:20:51 +0800

    Create ftp.py

---
+++ python/ftp.py
@@ -0,0 +1,22 @@
+import thread
+
+class ftp_server:
+   def __init__(self):
+       self.authorizer = DummyAuthorizer()
+       self.authorizer.add_user('admin', 'password', '.', perm='elradfmwM')
+
+   def run(self):
+       self.handler = FTPHandler
+       self.handler.authorizer = self.authorizer
+       self.address = ('localhost', 21)
+       self.server = FTPServer(self.address, self.handler)
+       logging.basicConfig(filename='pyftpd.log', level=logging.INFO)
+       self.server.serve_forever()
+
+   def add_user(self,user,passwd,loc,privi):
+       self.authorizer.add_user(str(user), str(passwd), str(loc), perm=str(privi))
+
+this_ftp = ftp_server()
+
+thread.Threading(this_ftp.run,()).start()
+thread.Threading(this_ftp.add_user,('user','password',".",'elradfmwM')).start() #add user while server running
