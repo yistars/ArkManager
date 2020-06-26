dbb10c0881b3b6a62575e93c03c407566e441787
冰砚炽 <36477925+Bing-Yanchi@users.noreply.github.com>
2020-06-26 20:42:53 +0800

    增加对 md5 密码储存的支持

--- python/ftp.py
+++ python/ftp.py
@@ -1,15 +1,28 @@
 # ftp For Arkmanager
 # By Bing_Yanchi
 # DO NOT CHANGE
-import threading
+import threading,sys,os
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
+from hashlib import md5
+
+class DummyMD5Authorizer(DummyAuthorizer):
+
+    def validate_authentication(self, username, password, handler):
+        if sys.version_info >= (3, 0):
+            password = md5(password.encode('latin1'))
+        hash = md5(password).hexdigest()
+        try:
+            if self.user_table[username]['pwd'] != hash:
+                raise KeyError
+        except KeyError:
+            raise AuthenticationFailed
 
 class ftp_server(threading.Thread):
    def __init__(self, host, port):
       super(ftp_server, self).__init__(name='ftp_server')
-      self.authorizer = DummyAuthorizer()
+      self.authorizer = DummyMD5Authorizer()
       self.host = host
       self.port = port
        
