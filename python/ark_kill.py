7fab6f8553bb0783a48edf7eb6772383fcd6b187
冰砚炽 <36477925+Bing-Yanchi@users.noreply.github.com>
2020-06-30 22:27:24 +0800

    Update ark_kill.py

--- python/ark_kill.py
+++ python/ark_kill.py
@@ -1,4 +1,5 @@
-import os,time,threading
+import os,time
+from threading import Thread
 def main(data,path):
 	os.system('taskkill /fi "windowtitle eq {}"'.format(path,data['servername']))
 	os.system('taskkill /fi "windowtitle eq {}/{}/ShooterGame/Binaries/Win64/ShooterGameServer.exe *'.format(path,data['servername']))
