910cc7c9ea16bc8dbae059e550636178d8d8f57c
冰砚炽 <Bing_Yanchi@outlook.com>
2020-07-13 10:35:10 +0800

    New translations zh_cn.php (Catalan)

---
+++ php/i18n/ca-ES.php
@@ -0,0 +1,120 @@
+<?php
+/* 简体中文 By iVampireSP */
+// $lang['indexTitle']
+$lang = array(
+    // index.php
+    'indexTitle' => '首页',
+    'indexHeader' => '控制面板',
+    'indexWelcome' => '欢迎使用控制面板',
+    'indexContent' => '请从左侧菜单选择操作。',
+    
+    // changepwd.php
+    'changepwdTitle' => '重置密码',
+    'changepwdHeader' => '重置密码',
+    'changepwdoldPassword' => '原密码',
+    'changepwdoldPasswordtip' => '请输入原密码！',
+    'changepwdnewPassword' => '新密码',
+    'changepwdnewPasswordtip' => '请输入新密码!',
+
+    // control.php
+    'controlTitle' => '控制台',
+    'controlHeader' => '控制台',
+    'controlh1' => '服务器控制台',
+    'controlstart' => '启动',
+    'controlstop' => '停止',
+    'controlc' => '请选择对：',
+    'controlc1' => '的操作',
+    'controlc2' => '在启动前请调整以下参数，我们不会保存以下参数。',
+    'controlc3' => '选择地图',
+    'controlc4' => '额外参数',
+
+    // ftp.php
+    'ftpTitle' => 'FTP',
+    'ftpHeader' => 'FTP',
+    'ftpT1' => 'FTP文件管理',
+    'ftpT2' => '您可以使用FTP来管理服务器上的文件',
+    'ftpT3' => '节点域名',
+    'ftpT4' => 'FTP端口',
+    'ftpT5' => '用户名',
+    'ftpT6' => '密码',
+
+    // login.php
+    'loginTitle' => '登录',
+    'loginHeader' => '登录',
+    'loginUsername' => '用户名',
+    'loginUsernanetip' => '用户名不能为空！',
+    'loginPassword' => '密码',
+    'loginPasswordtip' => '请输入密码！',
+
+    // Admin Login
+    'adminLogintitle' => '后台管理',
+    'adminLoginheader' => '后台管理',
+    'adminLoginpassword' => '密码',
+    'adminLoginpasswordtip' => '请输入密码',
+
+    // Admin Panel
+    'adminDashtitle' => '仪表盘',
+    'adminDashheader' => '仪表盘',
+    'adminDashT1' => '快速添加用户',
+    'adminDashT2' => '用户名',
+    'adminDashT3' => '密码',
+    'adminDashT4' => '快速删除用户',
+    'adminDashT5' => '用户名',
+
+    // Admin Node Manager
+    'adminNodemanagertitle' => 'Node Manager',
+    'adminNodemanagerHeader' => 'Node Manager',
+    'adminNodemanagerT1' => '集群节点管理',
+    'adminNodemanagerT2' => '欢迎使用Node Manager。',
+    'adminNodemanagerT3' => '节点名称',
+    'adminNodemanagerT4' => 'IP地址与端口（如localhost:1234）',
+    'adminNodemanagerT5' => 'Token',
+    'adminNodemanagerT6' => '取消',
+    'adminNodemanagerT7' => '添加',
+    'adminNodemanagerT8' => 'ID',
+    'adminNodemanagerT9' => '名称',
+    'adminNodemanagerT10' => 'IP:Port',
+    'adminNodemanagerT11' => 'Token',
+    'adminNodemanagerT12' => '操作',
+    'adminNodemanagerT13' => '添加节点',
+
+    // Admin Server Manager
+    'adminServermanagertitle' => 'Server Manager',
+    'adminServermanagerHeader' => '服务器管理',
+    'adminServermanagerT1' => '欢迎使用Server Manager。',
+    'adminServermanagerT2' => '服务器名称（尽量英文）',
+    'adminServermanagerT3' => '端口',
+    'adminServermanagerT4' => 'RCON端口(ARK的输出比较那个啥，所以只能用RCON。)',
+    'adminServermanagerT5' => 'Query端口(与Steam通信的端口)',
+    'adminServermanagerT6' => '最大玩家数量',
+    'adminServermanagerT7' => '属于节点',
+    'adminServermanagerT8' => '属于用户(填写用户ID)',
+    'adminServermanagerT9' => '到期日期(不填则不会到期)',
+    'adminServermanagerT10' => '取消',
+    'adminServermanagerT11' => '添加',
+    'adminServermanagerT12' => 'ID',
+    'adminServermanagerT13' => '名称',
+    'adminServermanagerT14' => '端口',
+    'adminServermanagerT15' => 'Query端口',
+    'adminServermanagerT16' => '最大玩家',
+    'adminServermanagerT17' => '节点',
+    'adminServermanagerT18' => '属于用户',
+    'adminServermanagerT19' => '到期日期',
+    'adminServermanagerT20' => '初始化',
+    'adminServermanagerT21' => '操作',
+    'adminServermanagerT22' => '创建服务器',
+    'adminServermanagerT23' => 'RCON端口',
+
+    // Admin User Manager
+    'adminUsermanagertitle' => 'User Manager',
+    'adminUsermanagerheader' => '用户管理',
+    'adminUsermanagerT1' => '欢迎使用User Manager',
+    'adminUsermanagerT2' => '用户名',
+    'adminUsermanagerT3' => '密码',
+    'adminUsermanagerT4' => '取消',
+    'adminUsermanagerT5' => '添加',
+    'adminUsermanagerT6' => 'ID',
+    'adminUsermanagerT7' => '用户名',
+    'adminUsermanagerT8' => '操作',
+    'adminUsermanagerT9' => '添加用户'
+);
