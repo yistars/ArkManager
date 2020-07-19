90ccef990fbfd6fd52dbe5d5e9fc1c75012a94f1
冰砚炽 <36477925+Bing-Yanchi@users.noreply.github.com>
2020-07-19 22:20:01 +0800

    修正 执行顺序

--- python/ark_init.py
+++ python/ark_init.py
@@ -4,10 +4,10 @@
 import os,time
 def main(path,servername):
     try:
-        os.system('robocopy {path}\ExampleServer {path}\{servername} /e && exit'.format(path=path,servername=servername))
         os.makedirs('{}/{}/sefolder'.format(path,servername))
         os.system('mklink /d "{path}/{servername}/sefolder/Content" "{path}/{servername}/ShooterGame/Content" && exit'.format(path=path,servername=servername))
         os.system('mklink /d "{path}/{servername}/sefolder/Saved" "{path}/{servername}/ShooterGame/Saved" && exit'.format(path=path,servername=servername))
+        os.system('robocopy {path}\ExampleServer {path}\{servername} /e && exit'.format(path=path,servername=servername))
     except:
         print('[E {}] [HTTP] Init Server {} error'.format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),servername))
     else:
