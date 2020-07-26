4bea2c7d1d311fedf1c400947315d7cf4694bf08
iVampireSP.com <im@ivampiresp.com>
2020-07-26 20:20:49 +0800

    增加加载条

--- php/config/theme.php
+++ php/config/theme.php
@@ -8,6 +8,10 @@ function mduiHeader($subtitle) {
             mdui-drawer="{target: '#main-drawer', swipe: true}"><i class="mdui-icon material-icons">menu</i></span>
         <a href="/index.php" class="mdui-typo-headline mdui-hidden-xs">$sitename</a>
         <a href="." class="mdui-typo-title">$subtitle</a>
+        <span onclick="change_style()" style="position: absolute; right: 5px; border-radius: 100%" mdui-tooltip="{content: '日夜配色', position: 'bottom'}" class="mdui-btn mdui-btn-icon mdui-ripple mdui-ripple-white"><i class="mdui-icon material-icons">&#xe3a9;</i></span>
+    </div>
+    <div id="topload" style="display: none; position: absolute; buttom: 10px;" class="mdui-progress">
+        <div class="mdui-progress-indeterminate"></div>
     </div>
 </header>
 EOF;
@@ -17,8 +21,7 @@ function mduiMenu() {
     echo <<<EOF
     <div class="mdui-drawer" id="main-drawer">
         <div class="mdui-list" mdui-collapse="{accordion: true}" style="margin-bottom: 76px;">
-        
-            <div class="mdui-collapse-item ">
+            <div class="mdui-collapse-item" id="menu">
                 <div class="mdui-collapse-item-header mdui-list-item mdui-ripple">
                     <i class="mdui-list-item-icon mdui-icon material-icons mdui-text-color-blue">near_me</i>
                     <div class="mdui-list-item-content">菜单</div>
@@ -46,14 +49,10 @@ function mduiHead($title) {
     <link rel="icon" href="https://ivampiresp.com/wp-content/uploads/2020/02/cropped-illust_78879291_20200207_181713-32x32.jpg" sizes="32x32" />
     <link rel="icon" href="https://ivampiresp.com/wp-content/uploads/2020/02/cropped-illust_78879291_20200207_181713-192x192.jpg" sizes="192x192" />
     <link rel="apple-touch-icon" href="https://ivampiresp.com/wp-content/uploads/2020/02/cropped-illust_78879291_20200207_181713-180x180.jpg" />
-    <style type="text/css">
-    .link {
-        color: blue;
-        text-decoration:none
-    }
-    </style>
+    <link rel="stylesheet" type="text/css" href="/assets/style.css" />
 EOF;
 }
 function mduiBody() {
-    echo '<body class="mdui-container mdui-drawer-body-left mdui-appbar-with-toolbar  mdui-theme-primary-indigo mdui-theme-accent-pink">';
+    echo '<body class="mdui-container mdui-drawer-body-left mdui-appbar-with-toolbar  mdui-theme-primary-indigo mdui-theme-accent-pink"><script type="text/javascript" src="/assets/script.js"></script>
+    <script src="https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js"></script>';
 }
