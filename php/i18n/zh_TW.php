02faa26c12b66efdec7c98b8bb40225138cc777c
冰砚炽 <Bing_Yanchi@outlook.com>
2020-10-08 22:39:41 +0800

    New translations zh_CN.php (Chinese Traditional)

---
+++ php/i18n/zh_TW.php
@@ -0,0 +1,91 @@
+<?php
+/* 简体中文 By iVampireSP */
+$lang = array(
+    // index.php
+    'index' => '首页',
+    'controlpanel' => '控制面板',
+    'welcome' => '欢迎使用控制面板',
+    'leftaction' => '在左侧菜单选择操作',
+
+    // changepwd.php
+    'changepwd' => '重置密码',
+    'oldpasswd' => '原密码',
+    'enteroldpasswd' => '请输入原密码！',
+    'newpasswd' => '新密码',
+    'enternewpasswd' => '请输入新密码！',
+    
+
+    // control.php
+    'console' => '控制台',
+    'serverconsole' => '服务器控制台',
+    'start' => '启动',
+    'stop' => '停止',
+    'doaction' => '控制服务器：',
+    'start_args' => '请在启动前调整参数，并且参数不会保存',
+    'select_map' => '选择地图',
+    'more_args' => '额外参数',
+    'update' => '更新',
+    'OrCustomMap' => '，或者您自行输入地图的名称：',
+    'likemodmap' => '如 Mod 里的地图',
+
+    // ftp.php
+    'filemanager' => 'FTP 文件管理',
+    'ftptip' => '您可以使用 FTP 来管理服务器上的文件',
+    'nodedomain' => '节点域名',
+    'port' => '端口',
+    'username' => '用户名',
+    'password' => '密码',
+
+    // login.php
+    'login' => '登录',
+    'enterusername' => '用户名不能为空！',
+    'pwd' => '密码',
+    'enterpwd' => '请输入密码！',
+
+    // Admin Login
+    'admindashboard' => '后台管理',
+
+    // Admin Panel
+    'menu' => '菜单',
+    'usermanager' => '用户管理',
+    'dashboard' => '仪表盘',
+    'quickadduser' => '快速添加用户',
+    'quickdeluser' => '快速删除用户',
+
+    // Admin Node Manager
+    'welcome' => '欢迎使用',
+    'nodemanager' => '集群节点管理',
+    'nodename' => '节点名称',
+    'ip_and_port' => 'IP 与端口 (如 localhost:1234)',
+    'cancel' => '取消',
+    'add' => '添加',
+    'name' => '名称',
+    'action' => '操作',
+    'addnode' => '添加节点',
+
+    // Admin Server Manager
+    'sever_manager' => '服务器管理',
+    'server_name_tip' => '服务器名称 (尽量使用英文)',
+    'port' => '端口',
+    'rcon_port_tip' => 'RCON 端口',
+    'query_port_tip' => 'Query 端口 (与 Steam 通信的端口)',
+    'by_node' => '属于节点',
+    'by_user_id' => '属于用户 (用户 ID)',
+    'exp_date_tip' => '到期日期 (不填则不会到期)',
+    'name' => '名称',
+    'maxplayers' => '最大玩家',
+    'node' => '节点',
+    'by_user' => '属于用户',
+    'exp_date' => '到期日期',
+    'init' => '初始化',
+    'create_server' => '创建服务器',
+    'rcon_port' => 'RCON 端口',
+    'query_port' => 'Query 端口',
+
+    // Admin User Manager
+    'adduser' => '添加用户',
+
+    // Admin Renew
+    'renew_date' => '新的到期日期，留空则为永久',
+    'confirm' => '确定'
+);
