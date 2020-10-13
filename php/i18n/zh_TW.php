12f281a3ef50b3d27322a6cb5ed89f548c82fd83
冰砚炽 <Bing_Yanchi@outlook.com>
2020-10-13 21:05:08 +0800

    New translations zh_CN.php (Chinese Traditional)

--- php/i18n/zh_TW.php
+++ php/i18n/zh_TW.php
@@ -11,21 +11,21 @@ $lang = array(
     'leftaction' => '在左侧菜单选择操作',
 
     // changepwd.php
-    'changepwd' => '重置密码',
-    'oldpasswd' => '原密码',
+    'changepwd' => '重設密碼',
+    'oldpasswd' => '舊密碼',
     'enteroldpasswd' => '请输入原密码！',
     'newpasswd' => '新密碼',
     'enternewpasswd' => '请输入新密码！',
     
     // control.php
     'console' => '控制台',
-    'serverconsole' => '服务器控制台',
-    'start' => '启动',
+    'serverconsole' => '伺服器主控台',
+    'start' => '啟動',
     'stop' => '停止',
     'doaction' => '控制服务器：',
     'start_args' => '请在启动前调整参数，并且参数不会保存',
-    'select_map' => '选择地图',
-    'more_args' => '额外参数',
+    'select_map' => '選擇地圖',
+    'more_args' => '額外參數',
     'update' => '更新',
     'OrCustomMap' => '或者您自行输入地图的名称：',
     'likemodmap' => '如 Mod 里的地图',
@@ -34,64 +34,64 @@ $lang = array(
     'filemanager' => 'FTP 文件管理',
     'ftptip' => '您可以使用 FTP 来管理服务器上的文件',
     'nodedomain' => '节点域名',
-    'port' => '端口',
+    'port' => '埠',
     'username' => '用户名',
-    'password' => '密码',
+    'password' => '密碼',
 
     // login.php
-    'login' => '登录',
+    'login' => '登入',
     'enterusername' => '用户名不能为空！',
-    'pwd' => '密码',
+    'pwd' => '密碼',
     'enterpwd' => '请输入密码！',
 
     // Admin Login
-    'admindashboard' => '后台管理',
+    'admindashboard' => '後台管理',
 
     // Admin Panel
-    'menu' => '菜单',
-    'usermanager' => '用户管理',
-    'dashboard' => '仪表盘',
+    'menu' => '選單',
+    'usermanager' => '使用者管理',
+    'dashboard' => '儀表板',
     'quickadduser' => '快速添加用户',
     'quickdeluser' => '快速删除用户',
 
     // Admin Node Manager
-    'welcome' => '欢迎使用',
+    'welcome' => '歡迎使用',
     'nodemanager' => '集群节点管理',
-    'nodename' => '节点名称',
+    'nodename' => '節點名稱',
     'ip_and_port' => 'IP 与端口 (如 localhost:1234)',
     'cancel' => '取消',
     'add' => '添加',
-    'name' => '名称',
+    'name' => '名稱',
     'action' => '操作',
-    'addnode' => '添加节点',
+    'addnode' => '增加節點',
 
     // Admin Server Manager
-    'sever_manager' => '服务器管理',
+    'sever_manager' => '伺服器管理',
     'server_name_tip' => '服务器名称 (尽量使用英文)',
-    'port' => '端口',
+    'port' => '埠',
     'rcon_port_tip' => 'RCON 端口',
     'query_port_tip' => 'Query 端口 (与 Steam 通信的端口)',
     'by_node' => '属于节点',
     'by_user_id' => '属于用户 (用户 ID)',
     'exp_date_tip' => '到期日期 (不填则不会到期)',
-    'name' => '名称',
+    'name' => '名稱',
     'maxplayers' => '最大玩家',
-    'node' => '节点',
+    'node' => '節點',
     'by_user' => '属于用户',
-    'exp_date' => '到期日期',
+    'exp_date' => '到期日',
     'init' => '初始化',
-    'create_server' => '创建服务器',
+    'create_server' => '創建伺服器',
     'rcon_port' => 'RCON 端口',
     'query_port' => 'Query 端口',
 
     // Admin User Manager
-    'adduser' => '添加用户',
+    'adduser' => '新增用戶',
 
     // Admin Renew
     'renew_date' => '新的到期日期，留空则为永久',
     'confirm' => '确定',
 
     // Server Config File
-    'edit' => '编辑',
-    'config_file' => '配置文件'
+    'edit' => '編輯',
+    'config_file' => '組態文件'
 );
