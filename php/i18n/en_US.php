ac0cb4ea17e97b809bc32a7abbdfe695969b5225
冰砚炽 <Bing_Yanchi@outlook.com>
2022-01-01 23:28:32 +0800

    New translations zh_CN.php (English)

--- php/i18n/en_US.php
+++ php/i18n/en_US.php
@@ -2,13 +2,13 @@
 $lang = array(
     // Global
     'language' => 'English',
-    'translator' => 'iVampireSP, Bing_Yanchi',
+    'translator' => 'iVampireSP, Bing_Yanchi, SakuraPuare',
 
     // index.php
     'index' => 'Home',
     'controlpanel' => 'Control Panel',
     'welcome' => 'Welcome to use control panel',
-    'leftaction' => '在左侧菜单选择操作',
+    'leftaction' => 'Choose an operation from the menu on left',
 
     // changepwd.php
     'changepwd' => 'Reset Password',
@@ -22,27 +22,27 @@ $lang = array(
     'serverconsole' => 'Server Console',
     'start' => 'Start',
     'stop' => 'Stop',
-    'doaction' => '控制服务器：',
-    'start_args' => '请在启动前调整参数，并且参数不会保存',
+    'doaction' => 'Control server:',
+    'start_args' => 'Please adjust the parameter before startup and the parameter will not be saved',
     'select_map' => 'Choose map',
     'more_args' => 'Extra parameters',
     'update' => 'Update',
-    'OrCustomMap' => '或者您自行输入地图的名称：',
-    'likemodmap' => '如 Mod 里的地图',
+    'OrCustomMap' => 'Or enter your custom map\'s name:',
+    'likemodmap' => 'Such as map in mod',
 
     // ftp.php
-    'filemanager' => 'FTP 文件管理',
-    'ftptip' => '您可以使用 FTP 来管理服务器上的文件',
-    'nodedomain' => '节点域名',
+    'filemanager' => 'FTP File Management',
+    'ftptip' => 'You can use FTP to manage files on this server',
+    'nodedomain' => 'Node Domain',
     'port' => 'Port',
     'username' => 'Username',
     'password' => 'Password',
 
     // login.php
     'login' => 'Login',
-    'enterusername' => '用户名不能为空！',
+    'enterusername' => 'The username cannot be empty!',
     'pwd' => 'Password',
-    'enterpwd' => '请输入密码！',
+    'enterpwd' => 'Please enter your password!',
 
     // Admin Login
     'admindashboard' => 'Admin',
@@ -51,14 +51,14 @@ $lang = array(
     'menu' => 'Menu',
     'usermanager' => 'User Management',
     'dashboard' => 'Dashboard',
-    'quickadduser' => '快速添加用户',
-    'quickdeluser' => '快速删除用户',
+    'quickadduser' => 'Quick Add User',
+    'quickdeluser' => 'Quick Delete User',
 
     // Admin Node Manager
     'welcome' => 'Welcome',
-    'nodemanager' => '集群节点管理',
+    'nodemanager' => 'Cluster Node Management',
     'nodename' => 'Node Name',
-    'ip_and_port' => 'IP 与端口 (如 localhost:1234)',
+    'ip_and_port' => 'IP address and server port (e.g. localhost:1234 )',
     'cancel' => 'Cancel',
     'add' => 'Add',
     'name' => 'Name',
@@ -67,28 +67,28 @@ $lang = array(
 
     // Admin Server Manager
     'sever_manager' => 'Server Manager',
-    'server_name_tip' => '服务器名称 (尽量使用英文)',
+    'server_name_tip' => 'Server name (use English as much as possible)',
     'port' => 'Port',
     'rcon_port_tip' => 'RCON Port',
-    'query_port_tip' => 'Query 端口 (与 Steam 通信的端口)',
-    'by_node' => '属于节点',
-    'by_user_id' => '属于用户 (用户 ID)',
-    'exp_date_tip' => '到期日期 (不填则不会到期)',
+    'query_port_tip' => 'Query port ( port communicate with Steam )',
+    'by_node' => 'belongs to node',
+    'by_user_id' => 'belongs to user (user ID)',
+    'exp_date_tip' => 'Expiry date (no expiration with empty)',
     'name' => 'Name',
     'maxplayers' => 'Max Players',
     'node' => 'Node',
-    'by_user' => '属于用户',
+    'by_user' => 'belongs to user',
     'exp_date' => 'Due Date',
     'init' => 'Initializing',
     'create_server' => 'Create server',
     'rcon_port' => 'RCON Port',
-    'query_port' => 'Query 端口',
+    'query_port' => 'Query Port',
 
     // Admin User Manager
     'adduser' => 'Add User',
 
     // Admin Renew
-    'renew_date' => '新的到期日期，留空则为永久',
+    'renew_date' => 'new expiry date, no expiration with empty',
     'confirm' => 'OK',
 
     // Server Config File
